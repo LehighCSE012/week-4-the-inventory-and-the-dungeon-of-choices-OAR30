8a0c8cfc15510738fff0cfe8c636526b9c727204
OAR30 <omr228@lehigh.edu>
2025-02-13 20:37:04 -0500

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -6,13 +6,13 @@ character encounters monsters, finds potions, and navigates
 based on user input.
 '''
 import random
-""" Adds item to inventory"""
 def acquire_item(inventory, item):
+    """ Adds item to inventory"""
     print(f"You acquired a {item}!")
     inventory.append(item)
     return inventory
-"""Displays items in inventory"""
 def display_inventory(inventory):
+    """Displays items in inventory"""
     count = 1
     if not inventory:
         print("Your inventory is empty.")
@@ -21,8 +21,9 @@ def display_inventory(inventory):
     for i in inventory:
         print(f"{count}. {i}")
         count += 1
-"""" 
-Displays Player Health
+def display_player_status(player_health):
+    """" 
+    Displays Player Health
 
     Parameters: player_health (int) - The current health of the player.
 
@@ -30,11 +31,10 @@ Displays Player Health
     For example: "Your current health: 100".
 
     Returns: Nothing.
-"""
-def display_player_status(player_health):
+    """
     print("Your current health:", player_health)
-"""Determines direction player goes in"""
 def handle_path_choice(player_health):
+    """Determines direction player goes in"""
     path_choice = random.choice(["left", "right"])
 
     if path_choice == "left":
@@ -53,14 +53,14 @@ def handle_path_choice(player_health):
             updated_player_health = player_health
             return updated_player_health
         return updated_player_health
-"""Player Attack Module"""
 def player_attack(monster_health):
+    """Player Attack Module"""
     monster_health -= 15
     print("You strike the monster for 15 damage!")
     updated_monster_health = int(monster_health)
     return updated_monster_health
-"""Monster Attack Module"""
 def monster_attack(player_health):
+    """Monster Attack Module"""
     result = random.choice([True, False])
     if result:
         player_health -= 10
@@ -71,8 +71,8 @@ def monster_attack(player_health):
     player_health -= 20
     updated_player_health = int(player_health)
     return updated_player_health
-"""Combat between player and monster"""  
 def combat_encounter(player_health, monster_health, has_treasure):
+    """Combat between player and monster"""
     while player_health > 0 and monster_health > 0:
         monster_health = player_attack(monster_health)
         if monster_health <= 0:
@@ -85,14 +85,14 @@ def combat_encounter(player_health, monster_health, has_treasure):
             break
         display_player_status(player_health)
     return has_treasure
-"""Checks for treasure"""
 def check_for_treasure(has_treasure):
+    """Checks for treasure"""
     if has_treasure == False:
         print("The monster did not have the treasure. You continue your journey.")
     else:
         print("You found the hidden treasure! You win!")
-"""Player enters different dungeons and discovers different items"""
 def enter_dungeon(player_health, inventory, dungeon_rooms):
+    """Player enters different dungeons and discovers different items"""
     print("You found a gold coins in the room.") #Work around for broken test, assignment requirements and test requirements differed
     for rooms in dungeon_rooms:
         print(rooms[0])
@@ -146,8 +146,8 @@ def enter_dungeon(player_health, inventory, dungeon_rooms):
             display_player_status(player_health)
             display_inventory(inventory)
     return player_health, inventory
-"""Main code function"""
 def main():
+    """Main code function"""
     inventory = []
     player_health_initial = 100
     monster_health_initial = 75
@@ -157,7 +157,8 @@ def main():
 
     current_player_health = handle_path_choice(player_health_initial) # Using player_health_initial
 
-    treasure_obtained_in_combat = combat_encounter(current_player_health, monster_health_initial, has_treasure) # Using monster_health_initial
+    treasure_obtained_in_combat = combat_encounter(current_player_health, monster_health_initial, has_treasure) 
+    # Using monster_health_initial
 
     check_for_treasure(treasure_obtained_in_combat) # Or has_treasure, depending on logic
 
